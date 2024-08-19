b2e509a216e29f2b08f55d1df9b9b99e748ea0cc
tnm0113 <tnm0113@gmail.com>
2024-08-19 10:54:48 +0700

    init

---
+++ db.ts
@@ -0,0 +1,45 @@
+import { Sequelize, DataType, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+});
+
+export const PositionInfoDB = sequelize.define(
+    'PositionInfo',
+    {
+        token0Name : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        token1Name : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        token0Amount: {
+            type: DataTypes.FLOAT
+        },
+        token1Amount: {
+            type: DataTypes.FLOAT
+        },
+        token0Value: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        },
+        token1Value: {
+            type: DataTypes.FLOAT,
+            defaultValue: 0
+        }
+    }
+)
+
+export async function connectToDB() {
+    try {
+        await sequelize.authenticate();
+        await PositionInfoDB.sync();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+}
+
